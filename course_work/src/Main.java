e444192936cd72309d5382f18bd2bb9e0edbb18e
Dmitry Ziganshin <z98dima@mail.ru>
2023-05-28 09:55:06 +0500

    cw v3

--- course_work/src/Main.java
+++ course_work/src/Main.java
@@ -1,39 +1,36 @@
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import com.opencsv.CSVReader;
+import java.util.*;
 
 public class Main {
-    public static List<String[]> readAll (Reader reader) throws Exception {
-        CSVReader csvReader = new CSVReader(reader);
-        List<String[]> list = new ArrayList<>();
-        return list = csvReader.readAll();
-    }
     public static void main(String[] args) throws Exception {
-        String path = "/Users/dmitryz/Desktop/java_ex/course_work/74040110_230429070000.csv";
-        String secondPath = "/Users/dmitryz/Desktop/java_ex/course_work/pdk_normatives.csv";
-        HashMap<String, String> hashMap = ReadCSV(path);
-        HashMap<String, Double> normativeMap = GetNormativeMap(secondPath);
-        System.out.println(hashMap.get("SO2"));
-        System.out.println(normativeMap.get("SO2"));
-    }
+        String path = "/Users/dmitryz/Desktop/java_ex/course_work/csv_files/74040110_230429070000.csv";
+        String secondPath = "/Users/dmitryz/Desktop/java_ex/course_work/csv_files/pdk_normatives.csv";
 
-    public static HashMap<String, String> ReadCSV (String path) {
+        LinkedHashMap<String, String> currentFile = readCSV(path);
+        LinkedHashMap<String, Double> normativeMap = getNormativeMap(secondPath);
+        LinkedHashMap<String, String> normativeNames = getNormativeList(secondPath);
+        List<String> troubleList = checkValues(currentFile, normativeMap, normativeNames);
+
+    }
+    
+    public static LinkedHashMap<String, String> readCSV (String path) {
         BufferedReader br = null;
         String cvsSplitBy = ";";
-        HashMap<String, String> hashMap = new HashMap<>();
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
         try {
             br = new BufferedReader(new FileReader(path));
             String[] keys = br.readLine().split(cvsSplitBy);
             String[] values = br.readLine().split(cvsSplitBy);
-            for (int i = 0; i < values.length; i++) {
-                hashMap.put(keys[i], values[i]);
+            for (int i = 0; i < keys.length; i++) {
+                if (i >= values.length) {
+                    linkedHashMap.put(keys[i], "");
+                }
+                else {
+                    linkedHashMap.put(keys[i], values[i]);
+                }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Ошибка: нет файла/неверно указан путь");
         } finally {
             if (br != null) {
                 try {
@@ -43,24 +40,65 @@ public class Main {
                 }
             }
         }
-        return hashMap;
+        return linkedHashMap;
     }
 
-    public static HashMap<String, Double> GetNormativeMap (String path) {
+    public static LinkedHashMap<String, Double> getNormativeMap (String path) {
         String csvSplitBy = ",";
         String line = "";
         String[] arrValues;
-        HashMap<String, Double> hashMap = new HashMap<>();
+        LinkedHashMap<String, Double> linkedHashMap = new LinkedHashMap<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             while ((line = br.readLine()) != null) {
                 arrValues = line.split(csvSplitBy);
                 if (arrValues[0].equals("id")) continue;
-                hashMap.put(arrValues[1], Double.parseDouble(arrValues[3]));
+                linkedHashMap.put(arrValues[1], Double.parseDouble(arrValues[3]));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return hashMap;
+        return linkedHashMap;
     }
+
+    public static LinkedHashMap<String, String> getNormativeList (String path) {
+        String csvSplitBy = ",";
+        String line = "";
+        String[] arrValues;
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+                arrValues = line.split(csvSplitBy);
+                if (arrValues[0].equals("id")) continue;
+                linkedHashMap.put(arrValues[1], arrValues[4]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return linkedHashMap;
+    }
+
+    public static List<String> checkValues (LinkedHashMap<String, String> file,
+                                            LinkedHashMap<String, Double> normatives,
+                                            LinkedHashMap<String, String> normativeNames) {
+        List<String> troubleList = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : normatives.entrySet()) {
+            if (entry.getKey().equals("T")) break;
+            String current = file.get(entry.getKey());
+            if (current == null || current.equals("")) continue;
+            if (Double.parseDouble(current) > entry.getValue()) {
+                String tmp = "Содержание " + entry.getKey() + " (" + normativeNames.get(entry.getKey()) + ") " +
+                        "на " + ((Double.parseDouble(current) / entry.getValue() * 100) - 100) + "% выше нормы";
+                troubleList.add(tmp);
+            }
+        }
+        return troubleList;
+    }
+
+    public static void makeFile (List<String> troubleList) {
+        
+    }
+
 }
+
